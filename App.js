4de15335972a5782a10dbb83bb41c3855a71aa3f
leslie.alldridge <leslie.alldridge@gmail.com>
2020-02-08 18:42:01 +1300

    render list

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {uuid} from 'uuidv4';
 import Header from './components/Header';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: uuid(),
+      text: 'Milk',
+    },
+    {
+      id: uuid(),
+      text: 'Eggs',
+    },
+    {
+      id: uuid(),
+      text: 'Bread',
+    },
+    {
+      id: uuid(),
+      text: 'Juice',
+    },
+  ]);
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
+      <FlatList
+        data={items}
+        renderItem={({item}) => <Text>{item.text}</Text>}
+      />
     </View>
   );
 };
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 
-const Header = props => {
+const Header = ({title}) => {
   return (
     <View style={styles.header}>
-      <Text style={styles.text}>{props.title}</Text>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
+};
+
+Header.defaultProps = {
+  title: 'Shopping List',
 };
 
 const styles = StyleSheet.create({
