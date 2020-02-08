8cb3e0281911da2eabdb07de5ec5c2e1044808b9
leslie.alldridge <leslie.alldridge@gmail.com>
2020-02-08 18:52:20 +1300

    items remove from shopping list

--- App.js
+++ App.js
@@ -24,12 +24,21 @@ const App = () => {
       text: 'Juice',
     },
   ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
+
   return (
     <View style={styles.container}>
       <Header title="Shopping List" />
       <FlatList
         data={items}
-        renderItem={({item}) => <ListItem item={item} />}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
       />
     </View>
   );
